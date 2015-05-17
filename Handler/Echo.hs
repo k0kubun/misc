57bce64b8dcd497ba1d7022ae4b2de09d008962c
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-05-17 16:28:36 +0900

    Do not write html directly in handler

--- Handler/Echo.hs
+++ Handler/Echo.hs
@@ -3,4 +3,4 @@ module Handler.Echo where
 import Import
 
 getEchoR :: Text -> Handler Html
-getEchoR theText = defaultLayout [whamlet|<h1>#{theText}|]
+getEchoR theText = defaultLayout $(widgetFile "echo")
