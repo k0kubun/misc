4cef3fbfa8433fb9428bafad3750e906d4e0ee48
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-05-17 16:38:37 +0900

    Implement mirror handler

--- Handler/Mirror.hs
+++ Handler/Mirror.hs
@@ -1,9 +1,12 @@
 module Handler.Mirror where
 
 import Import
+import qualified Data.Text as T
 
 getMirrorR :: Handler Html
-getMirrorR = error "Not yet implemented: getMirrorR"
+getMirrorR = defaultLayout $(widgetFile "mirror")
 
 postMirrorR :: Handler Html
-postMirrorR = error "Not yet implemented: postMirrorR"
+postMirrorR = do
+  postedText <- runInputPost $ ireq textField "content"
+  defaultLayout $(widgetFile "posted")
