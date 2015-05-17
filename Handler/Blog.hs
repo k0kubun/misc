e9917143ec35d920c0c7bbc6849306baa6ee20d4
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-05-17 18:56:03 +0900

    Add post action

--- Handler/Blog.hs
+++ Handler/Blog.hs
@@ -27,4 +27,13 @@ getBlogR = do
     $(widgetFile "articles")
 
 postBlogR :: Handler Html
-postBlogR = error "Not yet implemented: postBlogR"
+postBlogR = do
+  ((res, articleWidget), enctype) <- runFormPost entryForm
+  case res of
+    FormSuccess article -> do
+      articleId <- runDB $ insert article
+      setMessage $ toHtml $ (articleTitle article) <> " created"
+      redirect $ ArticleR articleId
+    _ -> defaultLayout $ do
+      setTitle "Please correct your entry form"
+      $(widgetFile "articleAddError")
