afdb784d0f38f4772cfa61066fc44c613a42f155
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-05-17 16:31:11 +0900

    Yesod add-handler mirror

---
+++ Handler/Mirror.hs
@@ -0,0 +1,9 @@
+module Handler.Mirror where
+
+import Import
+
+getMirrorR :: Handler Html
+getMirrorR = error "Not yet implemented: getMirrorR"
+
+postMirrorR :: Handler Html
+postMirrorR = error "Not yet implemented: postMirrorR"
