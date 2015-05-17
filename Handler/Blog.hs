25cb1cb9432999db789cb95cd7b45be881f1cb9e
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-05-17 16:52:57 +0900

    Yesod add-handler blog

---
+++ Handler/Blog.hs
@@ -0,0 +1,9 @@
+module Handler.Blog where
+
+import Import
+
+getBlogR :: Handler Html
+getBlogR = error "Not yet implemented: getBlogR"
+
+postBlogR :: Handler Html
+postBlogR = error "Not yet implemented: postBlogR"
