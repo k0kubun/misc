8eab4ca19433f716dd332e11b04baba4a079b7ac
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-05-17 15:14:44 +0900

    Implement getEchoR

--- Handler/Echo.hs
+++ Handler/Echo.hs
@@ -3,4 +3,4 @@ module Handler.Echo where
 import Import
 
 getEchoR :: String -> Handler Html
-getEchoR string = error "Not yet implemented: getEchoR"
+getEchoR theText = defaultLayout [whamlet|<h1>#{theText}|]
