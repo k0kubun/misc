9a7e4ed88f9e16cc649051e65630e09336a98811
Takashi Kokubun <takashi-kokubun@cookpad.com>
2015-05-18 21:31:10 +0900

    Create article show action

--- Handler/Article.hs
+++ Handler/Article.hs
@@ -3,4 +3,8 @@ module Handler.Article where
 import Import
 
 getArticleR :: ArticleId -> Handler Html
-getArticleR articleId = error "Not yet implemented: getArticleR"
+getArticleR articleId = do
+  article <- runDB $ get404 articleId
+  defaultLayout $ do
+    setTitle $ toHtml $ articleTitle article
+    $(widgetFile "article")
