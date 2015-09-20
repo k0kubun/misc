b4b28f229c1f75f1d2e8a0f68992571a384558a5
Takashi Kokubun <takashikkbn@gmail.com>
2015-09-20 12:27:21 +0900

    Print eather header

--- network/link/link.c
+++ network/link/link.c
@@ -66,8 +66,37 @@ int init_raw_socket(char *device, int promisc_flag, int ip_only)
     return soc;
 }
 
+// Convert MAC address to string
+char *my_ether_ntoa_r(u_char *hwaddr, char *buf, socklen_t size)
+{
+    snprintf(buf, size, "%02x:%02x:%02x:%02x:%02x:%02x",
+            hwaddr[0], hwaddr[1], hwaddr[2], hwaddr[3], hwaddr[4], hwaddr[5]);
+    return buf;
+}
+
 int print_ether_header(struct ether_header *eh, FILE *fp)
 {
+    char buf[80];
+
+    fprintf(fp, "ether_header------------------------\n");
+    fprintf(fp, "ether_dhost=%s\n", my_ether_ntoa_r(eh->ether_dhost, buf, sizeof(buf)));
+    fprintf(fp, "ether_shost=%s\n", my_ether_ntoa_r(eh->ether_shost, buf, sizeof(buf)));
+    fprintf(fp, "ether_type=%02X",  ntohs(eh->ether_type));
+
+    switch (ntohs(eh->ether_type)) {
+        case ETH_P_IP:
+            fprintf(fp, "(IP)\n");
+            break;
+        case ETH_P_IPV6:
+            fprintf(fp, "(IPv6)\n");
+            break;
+        case ETH_P_ARP:
+            fprintf(fp, "(ARP)\n");
+            break;
+        default:
+            fprintf(fp, "(unknown)\n");
+            break;
+    }
     return 0;
 }
 
