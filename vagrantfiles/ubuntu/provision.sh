caf3ce3ad472901fd7154dd5e35d4d9dd0a3caea
Takashi Kokubun <takashikkbn@gmail.com>
2015-09-24 19:47:59 +0900

    Provision with shell script

---
+++ vagrantfiles/ubuntu/provision.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo 'Installing Apache and setting it up...'
+apt-get update >/dev/null 2>&1
+apt-get install -y apache2 >/dev/null 2>&1
+rm -rf /var/www
+ln -fs /vagrant /var/www
