86e6c377fd0b1e8a17d67542dcd443a480e49263
Takashi Kokubun <takashikkbn@gmail.com>
2016-10-30 22:41:07 +0900

    Pass

--- spec/container_spec.rb
+++ spec/container_spec.rb
@@ -1,14 +1,5 @@
 require 'spec_helper'
 
-describe package('emacs') do
+describe package('bash') do
   it { should be_installed }
-end
-
-describe file('/var/www/html/index.php') do
-  it { should exist }
-end
-
-# php:5.6-apache が CMD で apache 起動しています
-describe service('apache2') do
-  it { should be_running }
 end
