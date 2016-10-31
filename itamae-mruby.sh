a493439c9949fd47d0affc9589bfe1b626fad847
Takashi Kokubun <takashi-kokubun@cookpad.com>
2016-10-31 10:44:44 +0900

    fix

--- itamae-mruby.sh
+++ itamae-mruby.sh
@@ -2,12 +2,12 @@
 
 set -ex
 
-itamae_version="0.2.1"
+mitamae_version="$1"
 
-rm -f /tmp/itamae-mruby-*
+rm -f /tmp/mitamae-*
 
-curl -o /tmp/itamae-mruby-linux.tar.gz -fL "https://github.com/k0kubun/itamae-mruby/releases/download/v${itamae_version}/itamae-x86_64-linux.tar.gz"
-curl -o /tmp/itamae-mruby-darwin.zip -fL   "https://github.com/k0kubun/itamae-mruby/releases/download/v${itamae_version}/itamae-x86_64-darwin.zip"
+curl -o /tmp/mitamae-linux.tar.gz -fL "https://github.com/k0kubun/mitamae/releases/download/v${mitamae_version}/mitamae-x86_64-linux.tar.gz"
+curl -o /tmp/imtamae-darwin.zip -fL   "https://github.com/k0kubun/mitamae/releases/download/v${mitamae_version}/mitamae-x86_64-darwin.zip"
 
-shasum -a 256 /tmp/itamae-mruby-linux.tar.gz
-shasum -a 256 /tmp/itamae-mruby-darwin.zip
+shasum -a 256 /tmp/mitamae-linux.tar.gz
+shasum -a 256 /tmp/mitamae-darwin.zip
