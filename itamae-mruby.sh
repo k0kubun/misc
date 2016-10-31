5cb985227aa1e92633ebfd9edef7dfb5885d7d9b
Takashi Kokubun <takashi-kokubun@cookpad.com>
2016-10-31 10:46:20 +0900

    typo

--- itamae-mruby.sh
+++ itamae-mruby.sh
@@ -7,7 +7,7 @@ mitamae_version="$1"
 rm -f /tmp/mitamae-*
 
 curl -o /tmp/mitamae-linux.tar.gz -fL "https://github.com/k0kubun/mitamae/releases/download/v${mitamae_version}/mitamae-x86_64-linux.tar.gz"
-curl -o /tmp/imtamae-darwin.zip -fL   "https://github.com/k0kubun/mitamae/releases/download/v${mitamae_version}/mitamae-x86_64-darwin.zip"
+curl -o /tmp/mitamae-darwin.zip -fL   "https://github.com/k0kubun/mitamae/releases/download/v${mitamae_version}/mitamae-x86_64-darwin.zip"
 
 shasum -a 256 /tmp/mitamae-linux.tar.gz
 shasum -a 256 /tmp/mitamae-darwin.zip
