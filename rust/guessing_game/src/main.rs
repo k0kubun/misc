8fd54e5ea6ceec2b47d15847c6e8d7b27c673bc5
Takashi Kokubun <takashikkbn@gmail.com>
2017-01-01 22:44:06 +0900

    Break lines

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -5,7 +5,13 @@ fn main() {
     println!("Please input your guess.");
 
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    let x = 5;
+    let y = 10;
+    println!("x and y: {} and {}", x, y);
 }
