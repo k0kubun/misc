2c92ca5a305d8087975cf4cc7b551bdb560cf6ea
Takashi Kokubun <takashikkbn@gmail.com>
2017-01-01 23:04:44 +0900

    Ignore error

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -5,10 +5,8 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
-
+    println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess:");
@@ -16,9 +14,10 @@ fn main() {
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        let guess: u32 = guess.trim()
-            .parse()
-            .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
