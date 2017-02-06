cb93a47110bfed6b949e5ca111741d65f7f962cb
Takashi Kokubun <takashikkbn@gmail.com>
2017-02-06 14:10:02 +0900

    temp

--- ruby/haml-bench/bench.rb
+++ ruby/haml-bench/bench.rb
@@ -7,10 +7,11 @@ Benchmark.ips do |x|
   haml_code = File.read("#{__dir__}/templates/view.haml")
   context = Object.new
 
-  Haml::Engine.new(haml_code, format: :html5, ugly: true, escape_html: true).def_method(context, :run_haml)
+  #Haml::Engine.new(haml_code, format: :html5, ugly: true, escape_html: true).def_method(context, :run_haml)
   context.instance_eval %Q[
     def run_faml; #{Faml::Engine.new.call(haml_code)}; end
     def run_hamlit; #{Hamlit::Engine.new.call(haml_code)}; end
+    def run_haml; #{File.read('haml_compiled.rb')}; end
   ]
 
   x.report("haml #{Haml::VERSION}")     { context.run_haml }
