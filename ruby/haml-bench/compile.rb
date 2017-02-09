a54c2418df27f1101e5c233a9eed809ffb2a19a7
Takashi Kokubun <takashikkbn@gmail.com>
2017-02-09 21:21:32 +0900

    Use buffer_defaults

--- ruby/haml-bench/compile.rb
+++ ruby/haml-bench/compile.rb
@@ -5,6 +5,10 @@ require 'hamlit'
 
 haml_code = File.read("#{__dir__}/templates/view.haml")
 
+buffer_options = { ugly: true, escape_html: true }
+if Haml::Options.respond_to?(:buffer_defaults)
+  Haml::Options.buffer_defaults.merge!(buffer_options)
+end
 haml_src   = Haml::Engine.new(haml_code, format: :html5, ugly: true, escape_html: true).compiler.precompiled_with_ambles([])
 # faml_src   = Faml::Engine.new.call(haml_code)
 hamlit_src = Hamlit::Engine.new.call(haml_code)
