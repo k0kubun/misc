9af6d7f5f794fc6ca94a1d4d4c518c9b4c744ff2
Takashi Kokubun <takashikkbn@gmail.com>
2017-02-12 13:26:05 +0900

    Update HelloWorld

---
+++ java/HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by k0kubun on 17/02/11.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
