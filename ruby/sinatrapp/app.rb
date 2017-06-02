24dcc8f48732d5afc4c7f2bbc4a4f6187674f419
Takashi Kokubun <takashikkbn@gmail.com>
2017-06-02 22:15:59 +0900

    Create haml example

--- ruby/sinatrapp/app.rb
+++ ruby/sinatrapp/app.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
+require 'haml'
 
-set :port, ENV.fetch('PORT', 8080)
+set :port, ENV.fetch('PORT', 3000)
 set :bind, '0.0.0.0'
 set :server, :puma
 
 get '/' do
-  'ok'
+  haml :index
 end
