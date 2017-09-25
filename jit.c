f4bd0df2afd408716b1054205de3bf7b940e6266
Takashi Kokubun <takashikkbn@gmail.com>
2017-09-25 22:48:34 +0900

    Always set value to stack variable
    This fixes JIT to execute missing method calls optimized away by current
    stack simuation.
    
    This change would fix many potential bugs...

--- jit.c
+++ jit.c
@@ -23,40 +23,6 @@ int jit_enabled = FALSE;
 /* Total count of scheduled ISeqs to generate unique identifier */
 static unsigned long jit_scheduled_iseqs;
 
-/* Emulates rb_control_frame's stack pointer */
-struct jit_stack {
-  unsigned int size;
-  unsigned int max;
-  char **body;
-};
-
-static void
-jit_stack_push(struct jit_stack *stack, char *value)
-{
-    if (stack->size >= stack->max) {
-	fprintf(stderr, "JIT internal stack overflow: max=%d, next size=%d", stack->max, stack->size+1);
-    }
-    stack->body[stack->size] = value;
-    stack->size++;
-}
-
-static char*
-jit_stack_pop(struct jit_stack *stack)
-{
-    if (stack->size <= 0) {
-	fprintf(stderr, "JIT internal stack underflow: next size=%d", stack->size-1);
-    }
-    stack->size--;
-    return stack->body[stack->size];
-}
-
-static char*
-jit_stack_topn(struct jit_stack *stack, unsigned int n)
-{
-  unsigned int last = stack->size - 1;
-  return stack->body[last - n];
-}
-
 static void
 execute_command(const char *path, char *const argv[])
 {
@@ -110,64 +76,17 @@ get_func_ptr(const char *so_fname, const char *funcname)
     return func_ptr;
 }
 
-/* Duplicates argument string in heap */
-static char *
-get_string(const char *str)
+static void
+fprint_call2(FILE *f, const char *func, unsigned int *stack_size)
 {
-    char *result;
-    if ((result = xmalloc(strlen(str) + 1)) != NULL) {
-	strcpy(result, str);
-    }
-    return result;
-}
-
-PRINTF_ARGS(static char *, 1, 2)
-get_stringf(const char *format, ...)
-{
-    char buf[1024];
-    va_list va;
-
-    va_start(va, format);
-    vsprintf(buf, format, va);
-    va_end(va);
-
-    return get_string(buf);
-}
-
-static char *
-get_value_string(VALUE value)
-{
-    return get_stringf("(VALUE)0x%"PRIxVALUE, value);
-}
-
-static char *
-get_call1_string(struct jit_stack *stack, const char *func)
-{
-    char *ret, *recv;
-    recv = jit_stack_pop(stack);
-    ret = get_stringf("%s(%s)", func, recv);
-
-    xfree(recv);
-    return ret;
-}
-
-static char *
-get_call2_string(struct jit_stack *stack, const char *func)
-{
-    char *ret, *recv, *obj;
-    obj = jit_stack_pop(stack);
-    recv = jit_stack_pop(stack);
-    ret = get_stringf("%s(%s, %s)", func, recv, obj);
-
-    xfree(obj);
-    xfree(recv);
-    return ret;
+    fprintf(f, "  stack[%d] = %s(stack[%d], stack[%d]);\n", *stack_size - 2, func, *stack_size - 2, *stack_size - 1);
+    (*stack_size)--;
 }
 
 static void
-compile_insn(FILE *f, struct jit_stack *stack, const int insn, const VALUE *operands)
+compile_insn(FILE *f, unsigned int *stack_size_ptr, const int insn, const VALUE *operands)
 {
-    char *value1, *value2;
+    unsigned int stack_size = *stack_size_ptr;
 
     switch (insn) {
       case YARVINSN_nop:
@@ -198,20 +117,20 @@ compile_insn(FILE *f, struct jit_stack *stack, const int insn, const VALUE *oper
       //case YARVINSN_setglobal:
       //  break;
       case YARVINSN_putnil:
-	jit_stack_push(stack, get_value_string(Qnil));
+	fprintf(f, "  stack[%d] = Qnil;\n", stack_size++);
         break;
       case YARVINSN_putself:
-	jit_stack_push(stack, get_string("cfp->self"));
+	fprintf(f, "  stack[%d] = cfp->self;\n", stack_size++);
         break;
       case YARVINSN_putobject:
-	jit_stack_push(stack, get_value_string(operands[0]));
+	fprintf(f, "  stack[%d] = (VALUE)0x%"PRIxVALUE";\n", stack_size++, operands[0]);
         break;
       //case YARVINSN_putspecialobject:
       //  break;
       //case YARVINSN_putiseq:
       //  break;
       case YARVINSN_putstring:
-	jit_stack_push(stack, get_stringf("rb_str_resurrect(0x%"PRIxVALUE")", operands[0]));
+	fprintf(f, "  stack[%d] = rb_str_resurrect(0x%"PRIxVALUE");\n", stack_size++, operands[0]);
         break;
       //case YARVINSN_concatstrings:
       //  break;
@@ -238,7 +157,7 @@ compile_insn(FILE *f, struct jit_stack *stack, const int insn, const VALUE *oper
       //case YARVINSN_newrange:
       //  break;
       case YARVINSN_pop:
-	xfree(jit_stack_pop(stack));
+	stack_size--;
         break;
       //case YARVINSN_dup:
       //  break;
@@ -264,8 +183,11 @@ compile_insn(FILE *f, struct jit_stack *stack, const int insn, const VALUE *oper
       //  break;
       case YARVINSN_trace:
 	fprintf(f, "  vm_dtrace((rb_event_flag_t)0x%"PRIxVALUE", th);\n", operands[0]);
-	fprintf(f, "  EXEC_EVENT_HOOK(th, (rb_event_flag_t)0x%"PRIxVALUE", cfp->self, 0, 0, 0, %s);\n", operands[0],
-		((rb_event_flag_t)operands[0] & (RUBY_EVENT_RETURN | RUBY_EVENT_B_RETURN)) ? jit_stack_topn(stack, 0) : "Qundef");
+	if ((rb_event_flag_t)operands[0] & (RUBY_EVENT_RETURN | RUBY_EVENT_B_RETURN)) {
+	    fprintf(f, "  EXEC_EVENT_HOOK(th, (rb_event_flag_t)0x%"PRIxVALUE", cfp->self, 0, 0, 0, stack[%d]);\n", operands[0], stack_size-1);
+	} else {
+	    fprintf(f, "  EXEC_EVENT_HOOK(th, (rb_event_flag_t)0x%"PRIxVALUE", cfp->self, 0, 0, 0, Qundef);\n", operands[0]);
+	}
         break;
       //case YARVINSN_trace2:
       //  break;
@@ -288,10 +210,8 @@ compile_insn(FILE *f, struct jit_stack *stack, const int insn, const VALUE *oper
       //case YARVINSN_invokeblock:
       //  break;
       case YARVINSN_leave:
-	value1 = jit_stack_pop(stack);
 	fprintf(f, "  th->ec.cfp = cfp+1;\n"); /* TODO: properly implement vm_pop_frame */
-	fprintf(f, "  return %s;\n", value1);
-	xfree(value1);
+	fprintf(f, "  return stack[%d];\n", stack_size-1); /* TODO: assert stack_size == 1 */
 	break;
       //case YARVINSN_throw:
       //  break;
@@ -314,51 +234,44 @@ compile_insn(FILE *f, struct jit_stack *stack, const int insn, const VALUE *oper
       //case YARVINSN_opt_case_dispatch:
       //  break;
       case YARVINSN_opt_plus:
-	jit_stack_push(stack, get_call2_string(stack, "vm_opt_plus")); /* TODO: handle Qundef */
+	fprint_call2(f, "vm_opt_plus", &stack_size); /* TODO: handle Qundef */
         break;
       case YARVINSN_opt_minus:
-	jit_stack_push(stack, get_call2_string(stack, "vm_opt_minus")); /* TODO: handle Qundef */
+	fprint_call2(f, "vm_opt_minus", &stack_size); /* TODO: handle Qundef */
         break;
       case YARVINSN_opt_mult:
-	jit_stack_push(stack, get_call2_string(stack, "vm_opt_mult")); /* TODO: handle Qundef */
+	fprint_call2(f, "vm_opt_mult", &stack_size); /* TODO: handle Qundef */
         break;
       case YARVINSN_opt_div:
-	jit_stack_push(stack, get_call2_string(stack, "vm_opt_div")); /* TODO: handle Qundef */
+	fprint_call2(f, "vm_opt_div", &stack_size); /* TODO: handle Qundef */
         break;
       case YARVINSN_opt_mod:
-	jit_stack_push(stack, get_call2_string(stack, "vm_opt_mod")); /* TODO: handle Qundef */
+	fprint_call2(f, "vm_opt_mod", &stack_size); /* TODO: handle Qundef */
         break;
       case YARVINSN_opt_eq:
-        value2 = jit_stack_pop(stack);
-        value1 = jit_stack_pop(stack);
-        jit_stack_push(stack, get_stringf("opt_eq_func(%s, %s, (CALL_INFO)0x%"PRIxVALUE", (CALL_CACHE)0x%"PRIxVALUE")",
-        	    value1, value2, operands[0], operands[1]));
-	xfree(value1);
-	xfree(value2);
+	fprintf(f, "stack[%d] = opt_eq_func(stack[%d], stack[%d], (CALL_INFO)0x%"PRIxVALUE", (CALL_CACHE)0x%"PRIxVALUE");\n",
+		stack_size-2, stack_size-2, stack_size-1, operands[0], operands[1]);
+	stack_size--;
         break;
       case YARVINSN_opt_neq:
-        value2 = jit_stack_pop(stack);
-        value1 = jit_stack_pop(stack);
-        jit_stack_push(stack,
-		get_stringf("vm_opt_neq((CALL_INFO)0x%"PRIxVALUE", (CALL_CACHE)0x%"PRIxVALUE", (CALL_INFO)0x%"PRIxVALUE", (CALL_CACHE)0x%"PRIxVALUE", %s, %s)",
-		    operands[0], operands[1], operands[2], operands[3], value1, value2));
-	xfree(value1);
-	xfree(value2);
+	fprintf(f, "stack[%d] = vm_opt_neq((CALL_INFO)0x%"PRIxVALUE", (CALL_CACHE)0x%"PRIxVALUE", (CALL_INFO)0x%"PRIxVALUE", (CALL_CACHE)0x%"PRIxVALUE", stack[%d], stack[%d]);\n",
+		stack_size-2, operands[0], operands[1], operands[2], operands[3], stack_size-2, stack_size-1);
+	stack_size--;
         break;
       case YARVINSN_opt_lt:
-	jit_stack_push(stack, get_call2_string(stack, "vm_opt_lt")); /* TODO: handle Qundef */
+	fprint_call2(f, "vm_opt_lt", &stack_size); /* TODO: handle Qundef */
         break;
       case YARVINSN_opt_le:
-	jit_stack_push(stack, get_call2_string(stack, "vm_opt_le")); /* TODO: handle Qundef */
+	fprint_call2(f, "vm_opt_le", &stack_size); /* TODO: handle Qundef */
         break;
       case YARVINSN_opt_gt:
-	jit_stack_push(stack, get_call2_string(stack, "vm_opt_gt")); /* TODO: handle Qundef */
+	fprint_call2(f, "vm_opt_gt", &stack_size); /* TODO: handle Qundef */
         break;
       case YARVINSN_opt_ge:
-	jit_stack_push(stack, get_call2_string(stack, "vm_opt_ge")); /* TODO: handle Qundef */
+	fprint_call2(f, "vm_opt_ge", &stack_size); /* TODO: handle Qundef */
         break;
       case YARVINSN_opt_ltlt:
-	jit_stack_push(stack, get_call2_string(stack, "vm_opt_ltlt")); /* TODO: handle Qundef */
+	fprint_call2(f, "vm_opt_ltlt", &stack_size); /* TODO: handle Qundef */
         break;
       //case YARVINSN_opt_aref:
       //  break;
@@ -375,30 +288,25 @@ compile_insn(FILE *f, struct jit_stack *stack, const int insn, const VALUE *oper
       //case YARVINSN_opt_empty_p:
       //  break;
       case YARVINSN_opt_succ:
-	jit_stack_push(stack, get_call1_string(stack, "vm_opt_succ")); /* TODO: handle Qundef */
+	fprintf(f, "stack[%d] = vm_opt_succ(stack[%d]);\n", stack_size-1, stack_size-1); /* TODO: handle Qundef */
         break;
       case YARVINSN_opt_not:
-        value1 = jit_stack_pop(stack);
-        jit_stack_push(stack, get_stringf("vm_opt_not((CALL_INFO)0x%"PRIxVALUE", (CALL_CACHE)0x%"PRIxVALUE", %s)",
-        	    operands[0], operands[1], value1));
-        xfree(value1);
+	fprintf(f, "stack[%d] = vm_opt_not((CALL_INFO)0x%"PRIxVALUE", (CALL_CACHE)0x%"PRIxVALUE", stack[%d]);\n",
+		stack_size-1, operands[0], operands[1], stack_size-1); /* TODO: handle Qundef */
         break;
       case YARVINSN_opt_regexpmatch1:
-        value1 = jit_stack_pop(stack);
-        jit_stack_push(stack,
-		get_stringf("vm_opt_regexpmatch1((VALUE)0x%"PRIxVALUE", %s)", operands[0], value1));
-        xfree(value1);
+	fprintf(f, "stack[%d] = vm_opt_regexpmatch1((VALUE)0x%"PRIxVALUE", stack[%d]);\n", stack_size-1, operands[0], stack_size-1);
         break;
       case YARVINSN_opt_regexpmatch2:
-	jit_stack_push(stack, get_call2_string(stack, "vm_opt_regexpmatch2")); /* TODO: handle Qundef */
+	fprint_call2(f, "vm_opt_regexpmatch2", &stack_size); /* TODO: handle Qundef */
         break;
       //case YARVINSN_opt_call_c_function:
       //  break;
       case YARVINSN_bitblt:
-	jit_stack_push(stack, get_string("rb_str_new2(\"a bit of bacon, lettuce and tomato\")"));
+	fprintf(f, "  stack[%d] = rb_str_new2(\"a bit of bacon, lettuce and tomato\");\n", stack_size++);
         break;
       case YARVINSN_answer:
-	jit_stack_push(stack, get_string("INT2FIX(42)"));
+	fprintf(f, "  stack[%d] = INT2FIX(42);\n", stack_size++);
         break;
       //case YARVINSN_getlocal_OP__WC__0:
       //  break;
@@ -409,16 +317,17 @@ compile_insn(FILE *f, struct jit_stack *stack, const int insn, const VALUE *oper
       //case YARVINSN_setlocal_OP__WC__1:
       //  break;
       case YARVINSN_putobject_OP_INT2FIX_O_0_C_:
-	jit_stack_push(stack, get_string("INT2FIX(0)"));
+	fprintf(f, "  stack[%d] = INT2FIX(0);\n", stack_size++);
         break;
       case YARVINSN_putobject_OP_INT2FIX_O_1_C_:
-	jit_stack_push(stack, get_string("INT2FIX(1)"));
+	fprintf(f, "  stack[%d] = INT2FIX(1);\n", stack_size++);
         break;
       default:
 	/* passing excessive arguments to suppress warning in insns_info.inc... */
 	fprintf(stderr, "Failed to compile instruction: %s (%s: %d...)\n", insn_name(insn), insn_op_types(insn), insn_len(insn) > 0 ? insn_op_type(insn, 0) : 0);
 	break;
     }
+    *stack_size_ptr = stack_size;
 }
 
 static void
@@ -426,27 +335,28 @@ compile_iseq_to_c(const struct rb_iseq_constant_body *body, FILE *f, const char 
 {
     unsigned int i;
     int insn;
-    struct jit_stack stack;
+    unsigned int stack_size;
 
-    fprintf(f, "#include \"jit_header.h\"\n");
+    fprintf(f, "#include \"jit_header.h\"\n\n");
     fprintf(f, "VALUE %s(rb_thread_t *th, rb_control_frame_t *cfp) {\n", funcname);
 
-    stack.size = 0;
-    stack.max  = body->stack_max;
-    stack.body = ALLOC_N(char *, body->stack_max);
+    if (body->stack_max > 0) {
+	fprintf(f, "  VALUE stack[%d];\n", body->stack_max);
+    }
 
+    stack_size = 0;
     for (i = 0; i < body->iseq_size;) {
 #if OPT_DIRECT_THREADED_CODE || OPT_CALL_THREADED_CODE
 	insn = rb_vm_insn_addr2insn((void *)body->iseq_encoded[i]);
 #else
 	insn = (int)body->iseq_encoded[i];
 #endif
-	compile_insn(f, &stack, insn, body->iseq_encoded + (i+1));
+	fprintf(f, "\n  /* %s */\n", insn_name(insn));
+	compile_insn(f, &stack_size, insn, body->iseq_encoded + (i+1));
+	/* TODO: check body->stack_max here */
 	i += insn_len(insn);
     }
     fprintf(f, "}\n");
-
-    xfree(stack.body);
 }
 
 void *
