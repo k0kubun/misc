5d18a6eb01457cfb267f4d136f0fd3a5b6e628c9
Takashi Kokubun <takashikkbn@gmail.com>
2018-04-06 23:57:44 +0900

    Update

--- ruby/benchrails/config/routes.rb
+++ ruby/benchrails/config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
-Rails.application.routes.draw do
   root 'welcome#index'
 end
