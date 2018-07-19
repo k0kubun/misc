991d264eba225b89bb735731bd818b9a2259bd24
Takashi Kokubun <takashikkbn@gmail.com>
2018-07-20 00:42:56 +0900

    Update dis

--- python/12sai/chap2.py
+++ python/12sai/chap2.py
@@ -1,4 +1,12 @@
-"""
-long comment
-"""
-print("15分は" + str(15 / 60) + "時間です")
+import dis
+
+def chap2():
+    """
+    long comment
+    """
+    # print("15分は" + str(15 / 60) + "時間です")
+    print(3 + 0.5)
+
+dis.dis(chap2)
+print("-" * 60)
+chap2()
