3e4e6b5e84c3e939170bca4387d23a738e3d3f37
Takashi Kokubun <takashikkbn@gmail.com>
2018-07-20 00:55:52 +0900

    Hour conversion

--- python/12sai/chap2.py
+++ python/12sai/chap2.py
@@ -1,12 +1,2 @@
-import dis
-
-def chap2():
-    """
-    long comment
-    """
-    # print("15分は" + str(15 / 60) + "時間です")
-    print(3 + 0.5)
-
-dis.dis(chap2)
-print("-" * 60)
-chap2()
+minutes = input("minutes?: ")
+print(minutes + " minutes equals to " + str(int(minutes) / 60) + "hours")
