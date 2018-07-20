caf3939c541d37f4d5c4f7602db499864d60c51e
Takashi Kokubun <takashikkbn@gmail.com>
2018-07-20 14:47:13 +0900

    show text label

--- python/12sai/chap3.py
+++ python/12sai/chap3.py
@@ -4,12 +4,17 @@ import tkinter
 root = tkinter.Tk()
 root.title("リリーにしつもん")
 root.minsize(640, 480)
+root.option_add("*font", ["", 11])
 
 # 画像表示
 canvas = tkinter.Canvas(bg="black", width=640, height=480)
 canvas.place(x=0, y=0)
 img = tkinter.PhotoImage(file="img3/chap3-back.png")
 canvas.create_image(320, 240, image=img)
+
+# テキスト表示
+question = tkinter.Label(text="知りたいのは何分かな?", bg="white")
+question.place(x=100, y=40)
 
 # メインループ
 root.mainloop()
