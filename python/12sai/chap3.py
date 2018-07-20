677192486add6da58ee1031d49ab6b47fffecee1
Takashi Kokubun <takashikkbn@gmail.com>
2018-07-20 15:17:56 +0900

    event driven

--- python/12sai/chap3.py
+++ python/12sai/chap3.py
@@ -16,5 +16,21 @@ canvas.create_image(320, 240, image=img)
 question = tkinter.Label(text="知りたいのは何分かな?", bg="white")
 question.place(x=100, y=40)
 
+# テキストボックス表示
+entry = tkinter.Entry(width=10, bd=4)
+entry.place(x=44, y=130)
+
+def ask_click():
+    print("ボタンがクリックされました!")
+
+# 質問ボタン表示
+askbutton = tkinter.Button(text="聞く")
+askbutton.place(x=258, y=128)
+askbutton["command"] = ask_click
+
+# 答え表示
+answer = tkinter.Label(text="……………", bg="white")
+answer.place(x=118, y=235)
+
 # メインループ
 root.mainloop()
