633e57cafaabed0261b1c48b5e5208d2603826b6
Takashi Kokubun <takashikkbn@gmail.com>
2018-10-01 00:45:29 +0900

    initial 1_1_a

--- java/aoj/Alds1_1_a.java
+++ java/aoj/Alds1_1_a.java
@@ -1,13 +1,25 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Alds1_1_a {
     public static void main(String[] args) {
-        InputStreamReader inputStream = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(inputStream);
-        try {
-            System.out.println(reader.readLine());
-        } catch (Exception e) {
+        Scanner scanner = new Scanner(System.in);
+        int len = Integer.parseInt(scanner.nextLine());
+
+        int inputs[] = new int[len];
+        for (int i = 0; i < len; i++) {
+            inputs[i] = Integer.parseInt(scanner.nextLine());
         }
+
+        new Alds1_1_a().printMax(inputs);
+    }
+
+    private static void printMax(int numbers[]) {
+        int max = 0;
+        for (int n : numbers) {
+            if (n > max) {
+                max = n;
+            }
+        }
+        System.out.println(max);
     }
 }
