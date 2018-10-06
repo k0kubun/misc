1cb16c9be9868e42a6d4ec6ec75584135ed8610f
Takashi Kokubun <takashikkbn@gmail.com>
2018-10-06 19:29:18 +0900

    Add practice

---
+++ java/atcoder/practice/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = Integer.parseInt(scanner.nextLine());
+        String[] bc = scanner.nextLine().split(" ", 2);
+        int b = Integer.parseInt(bc[0]);
+        int c = Integer.parseInt(bc[1]);
+        String s = scanner.nextLine();
+
+        System.out.print(a + b + c);
+        System.out.print(" ");
+        System.out.println(s);
+    }
+}
