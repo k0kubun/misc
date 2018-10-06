79bf06fb3d59f8abdafa1fc59f23b691cc63171a
Takashi Kokubun <takashikkbn@gmail.com>
2018-10-06 20:52:05 +0900

    1_2_b

---
+++ java/aoj/alds1_2_b/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int len = Integer.parseInt(scanner.nextLine());
+        String inputs[] = scanner.nextLine().split(" ", -1);
+
+        int numbers[] = new int[len];
+        for (int i = 0; i < len; i++) {
+            numbers[i] = Integer.parseInt(inputs[i]);
+        }
+
+        new Main().selectionSort(numbers, len);
+    }
+
+    private void selectionSort(int numbers[], int len) {
+        int times = 0;
+        boolean sorted = true;
+
+        for (int i = 0; i < len; i++) {
+            int minJ = i;
+            for (int j = i; j < len; j++) {
+                if (numbers[j] < numbers[minJ]) {
+                    minJ = j;
+                }
+            }
+
+            if (i != minJ) {
+                int temp = numbers[i];
+                numbers[i] = numbers[minJ];
+                numbers[minJ] = temp;
+                times++;
+            }
+        }
+
+        for (int i = 0; i < len; i++) {
+            System.out.print(numbers[i]);
+            if (i == len - 1) {
+                System.out.print("\n");
+            } else {
+                System.out.print(" ");
+            }
+        }
+        System.out.println(times);
+    }
+}
