875c3dba75425420a5c2b29df28a572501eb45b5
Takashi Kokubun <takashikkbn@gmail.com>
2018-10-06 21:07:56 +0900

    commit

---
+++ java/atcoder/abc112_b/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String firstLine[] = scanner.nextLine().split(" ", 2);
+        int n = Integer.parseInt(firstLine[0]);
+        int t = Integer.parseInt(firstLine[1]);
+
+        int cs[] = new int[n];
+        int ts[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            String line[] = scanner.nextLine().split(" ", 2);
+            cs[i] = Integer.parseInt(line[0]);
+            ts[i] = Integer.parseInt(line[1]);
+        }
+
+        int cost = new Main().minCost(n, t, cs, ts);
+        System.out.println(cost);
+    }
+
+    public int minCost(int n, int t, int costs[], int times[]) {
+        return 1;
+    }
+}
