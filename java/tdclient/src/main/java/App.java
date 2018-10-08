6484f342e0f9e702bff318e098edda68208044d5
Takashi Kokubun <takashikkbn@gmail.com>
2018-10-08 20:15:21 +0900

    remove old code

--- java/tdclient/src/main/java/App.java
+++
@@ -1,9 +0,0 @@
-import com.treasuredata.client.TDClient;
-
-public class App {
-    public static void main(String[] args) {
-        TDClient client = TDClient.newClient();
-        client.createBulkImportSession("test-session", "aaa", "bbb");
-        client.close();
-    }
-}
