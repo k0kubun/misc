7a554c7f9ec8bf0d1b7a95bc2a652d2c3c604641
Takashi Kokubun <takashikkbn@gmail.com>
2018-10-27 21:52:33 +0900

    temp

---
+++ java/atcoder/tenka1_2018_d.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long N = Long.parseLong(scanner.nextLine());
+    }
+}
