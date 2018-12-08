e746644f8b1a27394a4182bdd4c051b5a8b846bb
Takashi Kokubun <takashikkbn@gmail.com>
2018-12-08 22:10:42 +0900

    save

--- java/atcoder/abc115_d.java
+++ java/atcoder/abc115_d.java
@@ -1,6 +1,24 @@
 import java.util.*;
 
 class Main {
+    class Cache {
+        private long size;
+        private long pati;
+
+        public Cache(long size, long pati) {
+            this.size = size;
+            this.pati = pati;
+        }
+
+        public long size() {
+            return this.size;
+        }
+
+        public long pati() {
+            return this.pati;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] line = scanner.nextLine().split(" ", 2);
@@ -11,10 +29,12 @@ class Main {
 
     private long toEat;
     private long pati;
+    private HashMap<Long, Cache> cacheMap;
 
     public Main(long toEat, long pati) {
         this.toEat = toEat;
         this.pati = pati;
+        this.cacheMap = new HashMap<>();
     }
 
     public void main(long level) {
@@ -31,20 +51,38 @@ class Main {
             this.toEat--;
             this.pati++;
         } else {
+            Cache cache = this.cacheMap.get(level - 1);
+
+            long initToEat = this.toEat;
+            long initPati = this.pati;
+
             this.toEat--; // ban
 
-            if (this.toEat > 0) { // level - 1 burger
+            // level - 1 burger
+            if (cache != null && this.toEat >= cache.size) {
+                this.toEat -= cache.size;
+                this.pati += cache.pati;
+            } else if (this.toEat > 0) {
                 this.eat(level - 1);
             }
+
             if (this.toEat > 0) { // pati
                 this.toEat--;
                 this.pati++;
             }
-            if (this.toEat > 0) { // level - 1 burger
+
+            // level - 1 burger
+            if (cache != null && this.toEat >= cache.size) {
+                this.toEat -= cache.size;
+                this.pati += cache.pati;
+            } else if (this.toEat > 0) {
                 this.eat(level - 1);
             }
+
             if (this.toEat > 0) { // ban
                 this.toEat--;
+
+                this.cacheMap.put(level, new Cache(initToEat - toEat, pati - initPati));
             }
         }
     }
