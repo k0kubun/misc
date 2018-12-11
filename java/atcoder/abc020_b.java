6bd7ed6fe0cf41825e5e292f535f0cc584a504cd
Takashi Kokubun <takashikkbn@gmail.com>
2018-12-11 23:02:52 +0900

    Solve B problems

--- java/atcoder/abc020_b.java
+++ java/atcoder/abc020_b.java
@@ -3,5 +3,8 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] line = scanner.nextLine().split(" ", 2);
+        int ab = Integer.parseInt(String.format("%s%s", line[0], line[1]));
+        System.out.println(ab * 2);
     }
 }
--- java/atcoder/abc022_b.java
+++ java/atcoder/abc022_b.java
@@ -3,5 +3,17 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int count = 0;
+        boolean[] arrived = new boolean[100001];
+        for (int i = 0; i < n; i++) {
+            int type = Integer.parseInt(scanner.nextLine());
+            if (arrived[type]) {
+                count++;
+            }
+            arrived[type] = true;
+        }
+        System.out.println(count);
     }
 }
