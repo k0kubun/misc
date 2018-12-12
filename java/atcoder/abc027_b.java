a4fcd1d0bcebee16e06a2c6f14eb9dba151d86f4
Takashi Kokubun <takashikkbn@gmail.com>
2018-12-12 22:44:19 +0900

    Solve B problems

--- java/atcoder/abc027_b.java
+++ java/atcoder/abc027_b.java
@@ -3,5 +3,11 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        String[] line = scanner.nextLine().split(" ", n);
+
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(line[i]);
+        }
     }
 }
