ea0d45a91ca8415a3e500e7ec3c6ad58fd2a468b
Takashi Kokubun <takashikkbn@gmail.com>
2019-01-07 11:36:18 +0900

    full unwatch

---
+++ scripts/full-unwatch.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+begin
+  gem 'octokit'
+rescue Gem::LoadError
+  Gem.install('octokit')
+end
+require 'octokit'
+
+
+client = Octokit::Client.new(access_token: ENV.fetch('GITHUB_TOKEN'))
+client.auto_paginate = true
+
+repos = client.org_repos('treasure-data').to_a
+repos.each_with_index do |repository, index|
+  result = client.unwatch(repository[:full_name])
+  puts "[#{index + 1}/#{repos.size}] #{repository[:full_name]} => #{result}"
+end
