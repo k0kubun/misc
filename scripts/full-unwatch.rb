c04c315071ddee2f724e671c289b5222b462e1ff
Takashi Kokubun <takashikkbn@gmail.com>
2019-01-07 11:56:54 +0900

    fix broken script

--- scripts/full-unwatch.rb
+++ scripts/full-unwatch.rb
@@ -13,6 +13,6 @@ client.auto_paginate = true
 
 repos = client.org_repos('treasure-data').to_a
 repos.each_with_index do |repository, index|
-  result = client.unwatch(repository[:full_name])
+  result = client.delete_subscription(repository[:full_name])
   puts "[#{index + 1}/#{repos.size}] #{repository[:full_name]} => #{result}"
 end
