562696017c84f5df258b06caa36dcd06c8a9a6f2
Takashi Kokubun <takashikkbn@gmail.com>
2019-01-14 22:35:36 +0900

    Solve some C problems

--- java/atcoder/abc005_c.java
+++ java/atcoder/abc005_c.java
@@ -3,5 +3,43 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.nextLine());
+
+        int n = Integer.parseInt(scanner.nextLine());
+        String[] aLine = scanner.nextLine().split(" ", n);
+        int[] aArr = new int[n];
+        for (int i = 0; i < n; i++) {
+            aArr[i] = Integer.parseInt(aLine[i]);
+        }
+
+        int m = Integer.parseInt(scanner.nextLine());
+        String[] bLine = scanner.nextLine().split(" ", m);
+        int[] bArr = new int[m];
+        for (int i = 0; i < m; i++) {
+            bArr[i] = Integer.parseInt(bLine[i]);
+        }
+
+        int aInd = 0;
+        for (int bInd = 0; bInd < m; bInd++) {
+            if (aInd == n) {
+                System.out.println("no");
+                return;
+            }
+
+            while (aArr[aInd] + t < bArr[bInd]) {
+                aInd++;
+                if (aInd == n) {
+                    System.out.println("no");
+                    return;
+                }
+            }
+            if (aArr[aInd] <= bArr[bInd] && bArr[bInd] <= aArr[aInd] + t) {
+                aInd++;
+            } else {
+                System.out.println("no");
+                return;
+            }
+        }
+        System.out.println("yes");
     }
 }
--- java/atcoder/abc007_c.java
+++ java/atcoder/abc007_c.java
@@ -1,7 +1,71 @@
 import java.util.*;
 
 class Main {
-    public static void main(String[] args) {
+    class Point {
+        int r;
+        int c;
+
+        public Point(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    public void main() {
         Scanner scanner = new Scanner(System.in);
+        String[] rc = scanner.nextLine().split(" ", 2);
+        int r = Integer.parseInt(rc[0]);
+        int c = Integer.parseInt(rc[1]);
+
+        String[] sysx = scanner.nextLine().split(" ", 2);
+        int sy = Integer.parseInt(sysx[0]) - 1;
+        int sx = Integer.parseInt(sysx[1]) - 1;
+
+        String[] gygx = scanner.nextLine().split(" ", 2);
+        int gy = Integer.parseInt(gygx[0]) - 1;
+        int gx = Integer.parseInt(gygx[1]) - 1;
+
+        int[][] count = new int[r][c];
+        boolean[][] movable = new boolean[r][c];
+        for (int i = 0; i < r; i++) {
+            String line = scanner.nextLine();
+            for (int j = 0; j < c; j++) {
+                movable[i][j] = (line.charAt(j) == '.');
+                count[i][j] = -1;
+            }
+        }
+
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(new Point(sy, sx));
+        count[sy][sx] = 0;
+
+        Point point;
+        while ((point = queue.poll()) != null) {
+            if (point.r == gy && point.c == gx) {
+                System.out.println(count[point.r][point.c]);
+                return;
+            }
+
+            if (movable[point.r-1][point.c] && count[point.r-1][point.c] == -1) {
+                count[point.r-1][point.c] = count[point.r][point.c] + 1;
+                queue.add(new Point(point.r-1, point.c));
+            }
+            if (movable[point.r][point.c+1] && count[point.r][point.c+1] == -1) {
+                count[point.r][point.c+1] = count[point.r][point.c] + 1;
+                queue.add(new Point(point.r, point.c+1));
+            }
+            if (movable[point.r][point.c-1] && count[point.r][point.c-1] == -1) {
+                count[point.r][point.c-1] = count[point.r][point.c] + 1;
+                queue.add(new Point(point.r, point.c-1));
+            }
+            if (movable[point.r+1][point.c] && count[point.r+1][point.c] == -1) {
+                count[point.r+1][point.c] = count[point.r][point.c] + 1;
+                queue.add(new Point(point.r+1, point.c));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().main();
     }
 }
