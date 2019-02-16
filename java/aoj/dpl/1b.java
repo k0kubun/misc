95c58213ab881558f9a2e36b2f4049ac81d797a1
Takashi Kokubun <takashikkbn@gmail.com>
2019-02-17 00:26:56 +0900

    0-1 knapsack

--- java/aoj/dpl/1b.java
+++ java/aoj/dpl/1b.java
@@ -1,18 +1,60 @@
 import java.util.*;
 
+class Item {
+    int value;
+    int weight;
+
+    public Item(int value, int weight) {
+        this.value = value;
+        this.weight = weight;
+    }
+}
+
 class Main {
+    private static final int DIAGONAL = 1; // i is selected
+    private static final int TOP = 0; // i is NOT selected
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] nw = scanner.nextLine().split(" ", 2);
-        int N = Integer.parseInt(nw[0]);
-        int W = Integer.parseInt(nw[1]);
+        int n = Integer.parseInt(nw[0]);
+        int w = Integer.parseInt(nw[1]);
 
-        int[] v = new int[N];
-        int[] w = new int[N];
-        for (int i = 0; i < N; i++) {
+        Item[] items = new Item[n+1];
+        for (int i = 1; i <= n; i++) {
             String[] vw = scanner.nextLine().split(" ", 2);
-            v[i] = Integer.parseInt(vw[0]);
-            w[i] = Integer.parseInt(vw[1]);
+            int value = Integer.parseInt(vw[0]);
+            int weight = Integer.parseInt(vw[1]);
+            items[i] = new Item(value, weight);
         }
+
+        int maxValue = knapsackMaxValue(n, w, items);
+        System.out.println(maxValue);
+    }
+
+    private static int knapsackMaxValue(int n, int weight, Item[] items) {
+        int[][] maxValueC = new int[n+1][weight+1];
+        int[][] selectedG = new int[n+1][weight+1];
+
+        for (int w = 0; w <= weight; w++) {
+            maxValueC[0][w] = 0;
+            selectedG[0][w] = DIAGONAL;
+        }
+        for (int i = 1; i <= n; i++) maxValueC[i][0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int w = 1; w <= weight; w++) {
+                maxValueC[i][w] = maxValueC[i - 1][w];
+                selectedG[i][w] = TOP;
+                if (items[i].weight > w) continue;
+
+                if (items[i].value + maxValueC[i - 1][w - items[i].weight] > maxValueC[i - 1][w]) {
+                    maxValueC[i][w] = items[i].value + maxValueC[i - 1][w - items[i].weight];
+                    selectedG[i][w] = DIAGONAL;
+                }
+            }
+        }
+
+        return maxValueC[n][weight];
     }
 }
