76d08342e04c18e4cffb2746b95ae3b6fec80d6b
Takashi Kokubun <takashikkbn@gmail.com>
2019-02-17 18:59:11 +0900

    topological sort 2

--- java/atcoder/nikkei_d.java
+++ java/atcoder/nikkei_d.java
@@ -7,50 +7,63 @@ class Main {
         int n = Integer.parseInt(nm[0]);
         int m = Integer.parseInt(nm[1]);
 
-        Map<Integer, Integer> inputMap = new HashMap<>();
-        Map<Integer, Set<Integer>> parentsMap = new HashMap<>();
+        Map<Integer, List<Integer>> vParents = new HashMap<>();
+        Map<Integer, SortedSet<Integer>> vEdges = new HashMap<>();
         for (int i = 0; i < n - 1 + m; i++) {
             String[] ab = scanner.nextLine().split(" ", 2);
             int a = Integer.parseInt(ab[0]);
             int b = Integer.parseInt(ab[1]);
-            inputMap.put(a, b);
 
-            Set<Integer> parents = parentsMap.get(b);
+            SortedSet<Integer> edges = vEdges.get(a);
+            if (edges == null) {
+                edges = new TreeSet<>();
+                vEdges.put(a, edges);
+            }
+            edges.add(b);
+
+            List<Integer> parents = vParents.get(b);
             if (parents == null) {
-                parents = new HashSet<>();
-                parentsMap.put(b, parents);
+                parents = new ArrayList<>();
+                vParents.put(b, parents);
             }
             parents.add(a);
         }
 
-        for (int i = 1; i <= n; i++) {
-            if (!parentsMap.containsKey(i)) {
-                removeChildrensParent(inputMap, parentsMap, i, inputMap.get(i));
-                break;
-            }
+        List<Integer> sorted = new ArrayList<>();
+        Set<Integer> visited = new HashSet<>();
+        for (Integer v : vEdges.keySet()) {
+            topologicalSortVisit(sorted, visited, vEdges, v);
+        }
+
+        int[] rank = new int[n+1];
+        for (int i = 0; i < sorted.size(); i++) {
+            rank[sorted.get(i)] = i;
         }
 
         for (int i = 1; i <= n; i++) {
-            if (parentsMap.containsKey(i)) {
-                for (Integer parent : parentsMap.get(i)) {
-                    System.out.println(parent);
-                    break;
+            List<Integer> parents = vParents.get(i);
+            int maxParent = 0;
+            if (parents != null) {
+                for (Integer parent : parents) {
+                    if (maxParent == 0 || rank[maxParent] < rank[parent]) {
+                        maxParent = parent;
+                    }
                 }
-            } else {
-                System.out.println(0);
             }
+            System.out.println(maxParent);
         }
     }
 
-    private static void removeChildrensParent(Map<Integer, Integer> inputMap, Map<Integer, Set<Integer>> parentsMap, Integer removedParent, Integer parent) {
-        Integer child = inputMap.get(parent);
-        if (child == null) return;
+    private static void topologicalSortVisit(List<Integer> sorted, Set<Integer> visited,
+            Map<Integer, SortedSet<Integer>> vEdges, Integer v) {
+        if (visited.contains(v)) return;
+        visited.add(v);
 
-        Set<Integer> parentsSet = parentsMap.get(child);
-        if (parentsSet == null || parentsSet.size() == 1) return;
-
-        parentsSet.remove(removedParent);
-        removeChildrensParent(inputMap, parentsMap, removedParent, child);
-        removeChildrensParent(inputMap, parentsMap, parent, child);
+        if (vEdges.containsKey(v)) {
+            for (Integer nextV : vEdges.get(v)) {
+                topologicalSortVisit(sorted, visited, vEdges, nextV);
+            }
+        }
+        sorted.add(0, v);
     }
 }
