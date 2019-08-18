9f9240b9bde5d35b69166d43fbfcff6a8f3cc60d
Takashi Kokubun <takashikkbn@gmail.com>
2019-08-19 02:15:55 +0900

    Test nodejs

--- .github/actions/notify-slack/src/main.ts
+++ .github/actions/notify-slack/src/main.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core';
 
 async function run() {
   try {
-    const myInput = core.getInput('myInput');
+    const myInput = core.getInput('status');
     core.debug(`Hello ${myInput}`);
   } catch (error) {
     core.setFailed(error.message);
